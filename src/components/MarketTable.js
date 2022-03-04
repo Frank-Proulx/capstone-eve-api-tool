932698de61b0704fb53fd702bee03e84ba7505d1
Frank-Proulx <frankproulx7@gmail.com>
2022-03-04 10:59:38 -0800

    bugfix table display, figure out how to display remaining time

--- src/components/MarketTable.js
+++ src/components/MarketTable.js
@@ -3,27 +3,32 @@ import React from 'react';
 function MarketTable(props) {
   let sellTable;
   let buyTable;
+  function timifier(milliseconds) {
+
+  }
   if (props.sellOrders.length > 0) {
     sellTable = 
       <div>
         <table>
-          <tr>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Location</th>
-            <th>Range</th>
-            {/* <th>Min Volume</th> */}
-            <th>Expires In</th>
-          </tr>
-          {props.sellOrders.map((order, index) => {
-            <tr key={index}>
-              <td>{order.volue_remain}</td>
-              <td>{order.price}</td>
-              <td>{order.location_id}</td>
-              <td>{order.range}</td>
-              <td>{}</td>
+          <tbody>
+            <tr>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Location</th>
+              <th>Range</th>
+              {/* <th>Min Volume</th> */}
+              <th>Expires In</th>
             </tr>
-          })}
+            {props.sellOrders.map((order, index) => 
+              <tr key={index}>
+                <td>{order.volume_remain}</td>
+                <td>{order.price}</td>
+                <td>{order.location_id}</td>
+                <td>{order.range}</td>
+                <td>{7776000000 - (Date.now() - new Date(order.issued))}</td>
+              </tr>
+            )}
+          </tbody>
         </table>
       </div>;
   } else {
