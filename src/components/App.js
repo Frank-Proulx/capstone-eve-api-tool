bb394d28d5755723f67d844543f831e9739c912e
Frank-Proulx <frankproulx7@gmail.com>
2022-03-22 14:32:58 -0700

    add multiple background images and logic for display one at random

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Header from './Header';
 import MarketControl from './MarketControl';
-import spaceship from './../img/Eve-background.jpeg'
-
+import spaceship from './../img/Eve-background.jpeg';
+import dead from './../img/titan-dead.png';
+import green from './../img/titans-green.png'
 
 function App() {
 
+  let randomBack = Math.floor(Math.random() * 3);
+
+  if (randomBack === 0) {
+    randomBack = spaceship;
+  } else if (randomBack === 1) {
+    randomBack = dead;
+  } else {
+    randomBack = green;
+  }
+
   const backgroundImage = {
-    backgroundImage: `url(${spaceship})`,
+    backgroundImage: `url(${randomBack})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Header() {
+  let statusInfo;
+  const [status, setStatus] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://esi.evetech.net/latest/status/?datasource=tranquility
+    `)
+  .then(response => response.json())
+  .then(
+    (jsonifiedResponse) => {
+      setStatus(jsonifiedResponse)
+    })
+    .catch((error) => {
+      setStatus(error)
+    });
+  }, [])
 
   const headerStyle = {
     fontSize: "60px",
-    margin: "0 0 10px 0",
+    margin: "0",
     textAlign: "center",
     color: "white",
     paddingTop: "20px"
   }
 
+  const statusStyle = {
+    textAlign: "center",
+    fontSize: "20px",
+    color: "lightgreen"
+  }
+
+  status ? statusInfo = <p style={statusStyle}>Current players: {status.players}</p> : statusInfo = "Loading..."
+
   return (
     <React.Fragment>
       <h1 style={headerStyle}>Eve Online Interface</h1>
+      {statusInfo}
     </React.Fragment>
   );
 }
