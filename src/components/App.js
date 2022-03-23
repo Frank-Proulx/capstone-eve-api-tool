5fc642b8a2d469e93e319c34e1369acff9dbd50f
Frank-Proulx <frankproulx7@gmail.com>
2022-03-22 17:02:41 -0700

    add two more images for background options, change to switch statement

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,32 @@ import Header from './Header';
 import MarketControl from './MarketControl';
 import spaceship from './../img/Eve-background.jpeg';
 import dead from './../img/titan-dead.jpg';
-import green from './../img/titan-green.jpg'
+import green from './../img/titan-green.jpg';
+import charon from './../img/charon.jpg';
+import punisher from './../img/punisher.png';
 
 function App() {
 
-  let randomBack = Math.floor(Math.random() * 3);
+  let randomBack = Math.floor(Math.random() * 5);
 
-  if (randomBack === 0) {
-    randomBack = spaceship;
-  } else if (randomBack === 1) {
-    randomBack = dead;
-  } else {
-    randomBack = green;
+  switch (randomBack) {
+    case 0:
+      randomBack = spaceship;
+      break;
+    case 1:
+      randomBack = dead;
+      break;
+    case 2:
+      randomBack = green;
+      break;
+    case 3:
+      randomBack = charon;
+      break;
+    case 4:
+      randomBack = punisher;
+      break;
+    default:
+      console.log("there was an error with the background image")
   }
 
   const backgroundImage = {
